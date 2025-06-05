032d0178a56a41dc6c95d435997d083dcbb2971c
Arnaldo tomo <arnaldotomo@gmail.com>
2025-06-05 15:42:53 +0200

    Create config.js

---
+++ src/config/config.js
@@ -0,0 +1,12 @@
+// src/config/config.js
+export const API_BASE_URL = 'http://192.168.176.104:2021/api/v1';
+export const API_BASE_STORAGE = 'http://192.168.176.104:2021/storage/';
+
+// Configurações do app
+export const APP_CONFIG = {
+  name: 'FoodDelivery',
+  version: '1.0.0',
+  currency: 'MT',
+  defaultDeliveryFee: 25,
+  taxRate: 0.17, // 17% IVA
+};
